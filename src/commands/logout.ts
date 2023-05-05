63ed32ad21f75dccfa345e0c44dfd55cf4ace715
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-05-04 19:30:30 -0600

    add logout command

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -1,1 +1,16 @@
-export const logout = async () => {};
+import chalk from 'chalk';
+import { supabase } from '../supabase';
+
+export const logout = async () => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    console.log(chalk.red('You are not logged in!'));
+    return;
+  }
+
+  await supabase.auth.signOut();
+  console.log(chalk.green('Logged out successfully!'));
+};
