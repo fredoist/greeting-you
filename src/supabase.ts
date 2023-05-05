cee81da81534039e667ac7f5b7d86df84ed25f5e
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-05-05 00:18:45 -0600

    update deps and use alternative for settings

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,4 @@
 import { createCLISupabaseClient } from 'supabase-auth-helpers-cli';
-import { config } from 'dotenv';
+import { supabaseUrl, supabaseKey } from './supabase.json';
 
-config();
-const { SUPABASE_URL, SUPABASE_API_KEY } = process.env as {
-  SUPABASE_URL: string;
-  SUPABASE_API_KEY: string;
-};
-
-export const supabase = createCLISupabaseClient(SUPABASE_URL, SUPABASE_API_KEY);
+export const supabase = createCLISupabaseClient(supabaseUrl, supabaseKey);
