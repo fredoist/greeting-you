f81143c8b3042ab9dd55db809e8078548fcbfc45
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-05-04 21:54:38 -0600

    add delete command

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -1,1 +1,19 @@
-export const del = async () => {};
+import chalk from 'chalk';
+import { withAuth } from '../helpers';
+import { supabase } from '../supabase';
+
+export const del = withAuth(async ({ id }) => {
+  const { data, error } = await supabase.from('greetings').delete().match({ id }).select();
+
+  if (error) {
+    console.log(chalk.red(error.message));
+    return;
+  }
+
+  if (!data.length) {
+    console.log(chalk.red(`Unable to delete message ${id}!`));
+    return;
+  }
+
+  console.log(chalk.green(`Message ${id} deleted!`));
+});
